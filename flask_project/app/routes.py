785093069a976c2c60907b7055fc050226d5b368
durgaprasad411 <balijepallidurgaprasad94@gmail.com>
2025-05-01 13:24:07 +0530

    chnages1

--- flask_project/app/routes.py
+++ flask_project/app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 import os
 from werkzeug.utils import secure_filename
+from .models import User
 
 main = Blueprint('main', __name__)
 
@@ -30,11 +31,51 @@ def submit():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(UPLOAD_FOLDER, filename))
+        user = User(name=name, mobile=mobile, resume=filename)
         return jsonify({
             "message": "Data received successfully",
-            "name": name,
-            "mobile": mobile,
-            "resume": filename
+            "name": user.name,
+            "mobile": user.mobile,
+            "resume": user.resume
         }), 200
     else:
         return jsonify({"error": "File type not allowed"}), 400
+
+@main.route('/submit', methods=['GET'])
+def get_submit():
+    users = User.get_all_users()
+    return jsonify(users), 200
+
+@main.route('/submit/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    data = request.form
+    name = data.get('name')
+    mobile = data.get('mobile')
+
+    if 'resume' in request.files:
+        file = request.files['resume']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(UPLOAD_FOLDER, filename))
+        else:
+            return jsonify({"error": "File type not allowed"}), 400
+    else:
+        filename = None
+
+    user = User.update_user(user_id, name, mobile, filename)
+    if user:
+        return jsonify({
+            "message": "User updated successfully",
+            "name": user.name,
+            "mobile": user.mobile,
+            "resume": user.resume
+        }), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@main.route('/submit/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    if User.delete_user(user_id):
+        return jsonify({"message": "User deleted successfully"}), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
